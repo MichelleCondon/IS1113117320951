1511d475ca4ed387296218afec80da392b30398d
Michelle Condon <117320951@umail.ucc.ie>
2018-02-07 11:17:03 +0000

    Updating code

--- ebusiness/cost_calc.js
+++ ebusiness/cost_calc.js
@@ -1,30 +1,36 @@
 /* global $ */
+//We must calculate VAT and discount ourself
 
-function calcSub(){
-    var argSubTotal;
+function calcSub()
+{
+    var argSubTotal; //Creates Variable
     
-    if(document.getElementById('salesforce').checked) {
-        argSubTotal=100;
+    if(document.getElementById('salesforce').checked)
+    {
+        argSubTotal = 100; //If salesforce is checked the variable = 100
     }
-    else{
-        argSubTotal=300;
+    else
+    {
+        argSubTotal = 300; //Other option has a value of 300
     }
-    display(argSubTotal);
+    display(argSubTotal) //Passing variable in the display function
 }
 
-function display(parm1){
+function display(parm1) //parm1 equals value of argSubTotal
+{
     
     document.getElementById("subtotal").value = parm1;
     document.getElementById("total").value = parm1;
     
     enablebtnProceed();
-    
 }
 
-function enablebtnProceed(){
-    $('#btnProceed').prop('disabled',false);
-    
+function enablebtnProceed()
+{
+    $('#btnProceed').prop('disabled', false);//find btnProceed go into properties and enable btn
 }
-function disablebtnProceed(){
-    $('#btnProceed').prop('disabled',true);
+
+function disablebtnProceed()
+{
+    $('#btnProceed').prop('disabled', true);
 }
