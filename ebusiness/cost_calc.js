a8ab0441335cf610b52155081fc3ca3d066375e6
Michelle Condon <117320951@umail.ucc.ie>
2018-02-08 14:43:15 +0000

    Calculations on products

--- ebusiness/cost_calc.js
+++ ebusiness/cost_calc.js
@@ -1,18 +1,41 @@
 /* global $ */
-//We must calculate VAT and discount ourself
-function calcSub()
+
+function calcSub()// Creating a function to calculate
 {
-    var argSubTotal; //Creates Variable
+    var argSubTotal;
+    var argDiscount;
+    var argVat;
+    var argTotal;//Creates Variable
     
     if(document.getElementById('salesforce').checked)
     {
-        argSubTotal = 100; //If salesforce is checked the variable = 100
-    }
-    else
+        argSubTotal = 100;
+        argDiscount = argSubTotal * 0.05;
+        argVat = (argSubTotal - argDiscount) * 0.10;
+        argTotal = (argSubTotal - argDiscount) + argVat; //If salesforce is checked the variable = 100
+    } else if(document.getElementById('cloud 9').checked)
     {
-        argSubTotal = 300; //Other option has a value of 300
+        argSubTotal = 200; //If cloud 9 is checked the variable = 200
+        argDiscount = argSubTotal * 0.05;
+        argVat = (argSubTotal - argDiscount) * 0.10;
+        argTotal = (argSubTotal - argDiscount) + argVat;
+    } else if(document.getElementById('aws').checked)
+    {
+        argSubTotal = 300;//If amazon web services is checked the variable = 300
+        argDiscount = argSubTotal * 0.05;
+        argVat = (argSubTotal - argDiscount) * 0.10;
+        argTotal = (argSubTotal - argDiscount) + argVat;
+    } else {
+        argSubTotal = 400; //Gmail has a value of 400
+       argDiscount = argSubTotal * 0.05;
+        argVat = (argSubTotal - argDiscount) * 0.10;
+        argTotal = (argSubTotal - argDiscount) + argVat;
     }
-    display(argSubTotal) //Passing variable in the display function
+    display(argSubTotal);
+    show(argDiscount);
+    view(argVat);
+    determine(argTotal);
+    //Passing variable in the display function
 }
 
 function display(parm1) //parm1 equals value of argSubTotal
@@ -23,6 +46,30 @@ function display(parm1) //parm1 equals value of argSubTotal
     
     enablebtnProceed();
 }
+function show(parm1) //parm1 equals value of argSubTotal
+{
+    
+    document.getElementById("discount").value = parm1;
+    
+    enablebtnProceed();
+}
+
+function view(parm1) //parm1 equals value of argSubTotal
+{
+    
+    document.getElementById("vat").value = parm1;
+    
+    enablebtnProceed();
+}
+
+function determine(parm1) //parm1 equals value of argSubTotal
+{
+    
+    document.getElementById("total").value = parm1;
+    
+    enablebtnProceed();
+}
+
 
 function enablebtnProceed()
 {
