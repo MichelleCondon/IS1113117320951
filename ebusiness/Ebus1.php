af58057bb4b523b77d51504d69edadb653770f5f
Michelle Condon <117320951@umail.ucc.ie>
2018-02-08 10:00:42 +0000

    Ebus3

--- ebusiness/Ebus1.php
+++ ebusiness/Ebus1.php
@@ -1,79 +1,91 @@
 <!DOCTYPE html>
-<html>  
-    <head>  
+<html>
+    <head>
         <title>Select Product</title>
         
-        <!--JQuery-->
-        <script src="https://ajax.googleleapis/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="text/javascript" src="cost_calc.js"></script>
-    
-    <style>
-header{
-    background-color:#00ace6;
-    color:white;
-}
-</style>
+        <!--jQuery-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script type="text/javascript" src="cost_calc.js"></script>
+
     </head>
     
-    
     <body>
-    <center>
+        <center>
         <header>
-        <h1>Select a Product</h1>
-        </header>
-        <br/>
-        
-        <form method="POST" action="Ebus2.php">
-            
-           <label for="Salesforce">
-               <input type="radio" id="salesforce" name="product" checked onClick="disablebtnProceed()"/>
-               SalesForce @ $100
-           </label>
-            
+            <h4>Select a Product</h4>
+            </header>
             <br/>
             
-            <label for="aws">
-               <input type="radio" id="aws" name="product"  checked onClick="disablebtnProceed()"/>
+            <form method="POST" action="Ebus2.php">
+              
+              <label for="salesforce">
+                <input type="radio" id="salesforce" name="product" checked onClick="disablebtnProceed()"/>
+                SalesForce @ $100
+              </label>
+              
+              <br/>
+              <br/>
+              
+              <label for="cloud 9">
+                  <input type="radio" id="cloud 9" name="product" checked onClick="disablebtnProceed()"/>
+                  Cloud 9 @ $200
+              </label>
+              
+              <br/>
+              <br/>
+              
+              <label for="aws">
+                <input type="radio" id="aws" name="product" onClick="disablebtnProceed()"/>
                 Amazon Web Services @ $300
-           </label>
-           
-           <br/>
-           <label for="cloud9">
-               <input type="radio" id="cloud9" name="product"  checked onClick="disablebtnProceed()"/>
-                Cloud 9 @ $200
-           </label>
-           
-           <br/>
-           <label for="Gmail">
-               <input type="radio" id="Gmail" name="product"  checked onClick="disablebtnProceed()"/>
-                Gmail @ $400
-           </label>
-           <br/>
-           <br/>
-           
-           
-            <label for="subtotal">
-                Sub total
-               <input type="text" id="subtotal" value="0.00" readonly/>
-           </label>
-           
-           <br/>
-           
-            
-            <label for="total">
+              </label>
+              
+              <br/>
+              <br/>
+              
+              <label for="gmail">
+                  <input type="radio" id="gmail" name="product" onClick="disablebtnProceed()"/>
+                  Gmail @ $400
+              </label>
+              
+              <br/>
+              <br/>
+              
+              <label for="subtotal">
+                Sub Total
+                <input type="text" id="subtotal" value="0.00" readonly/>
+              </label>
+              
+              <br/>
+              <br/>
+              
+              <label for="discount">
+                  Discount @ 5%
+                  <input type="text" id="discount" value=0.00 readonly/>
+              </label>
+              
+              <br/>
+              <br/>
+              
+              <label for ="vat">
+                  Vat @ 10%
+                  <inpput type="text" id=vat value=0.00 readonly/>
+              </label>
+              
+              <label for="total">
                 Total
-               <input type="text" id="total" value="0.00" readonly/>
-           </label>
-           
-           <br/>
-           
-           <button type="submit" id="btnProceed" disabled>Add to Shopping Cart</button>
-           
-           </form>
-           
-           <br/>
-           <button onClick="calcSub()">Calculate Cost</button>
-           <a role="button" href="Ebus1.php">Clear Choice</a>
-        </center>
+                <input type="text" id="total" value="0.00" readonly/>
+              </label>
+    
+              <br/>
+              
+              <button type="submit" id="btnProceed" disabled>Add to Shopping Cart</button>
+            
+            </form>
+            
+            <br/>
+            <button onClick="calcSub()">Calculate Cost</button>
+            <a role="button" href="Ebus1.php">Clear Choice</a>
+            
+    </center>
     </body>
 </html>
--- ebusiness/cost_calc.js
+++ ebusiness/cost_calc.js
@@ -1,26 +1,27 @@
 /* global $ */
 //We must calculate VAT and discount ourself
-
 function calcSub()
 {
     var argSubTotal; //Creates Variable
     
-    if (document.getElementById('Salesforce').checked)
+    if(document.getElementById('salesforce').checked)
     {
         argSubTotal = 100; //If salesforce is checked the variable = 100
     }
-    else {
+    else
+    {
         argSubTotal = 300; //Other option has a value of 300
     }
-    display(argSubTotal); //Passing variable in the display function
+    display(argSubTotal) //Passing variable in the display function
 }
+
 function display(parm1) //parm1 equals value of argSubTotal
 {
     
     document.getElementById("subtotal").value = parm1;
     document.getElementById("total").value = parm1;
     
-     enablebtnProceed();
+    enablebtnProceed();
 }
 
 function enablebtnProceed()
@@ -32,3 +33,4 @@ function disablebtnProceed()
 {
     $('#btnProceed').prop('disabled', true);
 }
+   
