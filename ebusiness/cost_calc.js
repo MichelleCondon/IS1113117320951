c565526b2ab51d42e2d346641c8c897c22924414
Michelle Condon <117320951@umail.ucc.ie>
2018-02-10 22:26:15 +0000

    calculations

--- ebusiness/cost_calc.js
+++ ebusiness/cost_calc.js
@@ -1,76 +1,70 @@
 /* global $ */
 
-function calcSub()// Creating a function to calculate
+function calcSub(argSubTotal)// Creating a function to calculate
 {
     var argSubTotal;
     var argDiscount;
     var argVat;
     var argTotal;
+    var discountAmt;
+    var vatAmt;
+    var totalprice;
     
     if(document.getElementById('salesforce').checked)
     {
         argSubTotal = 100;
-        argDiscount = argSubTotal * 0.05;
-        argVat = (argSubTotal - argDiscount) * 0.10;
-        argTotal = (argSubTotal - argDiscount) + argVat;
-        display(argSubTotal,argDiscount,argVat,argTotal);
+        
     } else if(document.getElementById('cloud 9').checked)
     {
         argSubTotal = 200;//If cloud 9 is checked the variable = 200
-        argDiscount = argSubTotal * 0.05;
-        argVat = (argSubTotal - argDiscount) * 0.10;
-        argTotal = (argSubTotal - argDiscount) + argVat;
-        display(argSubTotal,argDiscount,argVat,argTotal);
+        
       
     } else if(document.getElementById('aws').checked)
     {
         argSubTotal = 300;//If amazon web services is checked the variable = 300
-        argDiscount = argSubTotal * 0.05;
-        argVat = (argSubTotal - argDiscount) * 0.10;
-        argTotal = (argSubTotal - argDiscount) + argVat;
-        display(argSubTotal,argDiscount,argVat,argTotal);
+        
     } else {
         argSubTotal = 400; //Gmail has a value of 400
-        argDiscount = argSubTotal * 0.05;
-        argVat = (argSubTotal - argDiscount) * 0.10;
-        argTotal = (argSubTotal - argDiscount) + argVat;
-        display(argSubTotal,argDiscount,argVat,argTotal);
+        
     }
-    display(argSubTotal);
-    show(argDiscount);
-    clear(argVat);
-    determine(argTotal);
-    //Passing variable in the display function
+  calcDisVatTotal(argSubTotal)
 }
-function display(parm1) //parm1 equals value of argSubTotal
+function calcDisVatTotal(parmSubTotal){
+    var discountAmt;
+    var vatAmt;
+    var totalprice;
+    var argSubTotal;
+     if(document.getElementById('salesforce').checked)
+    {
+        discountAmt = argSubTotal * 0.05;
+        vatAmt = (argSubTotal - discountAmt) * 0.10;
+        totalprice = (argSubTotal - discountAmt) + vatAmt;
+    } else if(document.getElementById('cloud 9').checked)
+    {
+        discountAmt = argSubTotal * 0.05;
+        vatAmt = (argSubTotal - discountAmt) * 0.10;
+        totalprice = (argSubTotal - discountAmt) + vatAmt;
+    } else if(document.getElementById('aws').checked)
+    {
+        discountAmt = argSubTotal * 0.05;
+        vatAmt = (argSubTotal - discountAmt) * 0.10;
+        totalprice = (argSubTotal - discountAmt) + vatAmt;
+    } else {
+        discountAmt = argSubTotal * 0.05;
+        vatAmt = (argSubTotal - discountAmt) * 0.10;
+        totalprice = (argSubTotal - discountAmt) + vatAmt;
+        display(subTotal,discountAmt,vatAmt,totalprice)
+}
+function display(parm1,parm2,parm3,parm4) //parm1 equals value of argSubTotal
 {
     
     document.getElementById("subtotal").value = parm1;
+    document.getElementById("discountAmt").value = parm2;
+    document.getElementById("vatAmt").value = parm3;
+    document.getElementById("totalprice").value = parm4;
     
     enablebtnProceed();
 }
-function show(parm2) //parm1 equals value of argSubTotal
-{
-    
-    document.getElementById("discount").value = parm2;
-    
-    enablebtnProceed();
-}
-function clear(parm3) //parm1 equals value of argSubTotal
-{
-    
-    document.getElementById("vat").value = parm3;
-    
-    enablebtnProceed();
-}
-function determine(parm4) //parm1 equals value of argSubTotal
-{
-    
-    document.getElementById("total").value = parm4;
-    
-    enablebtnProceed();
-}
-
 function enablebtnProceed()
 {
     $('#btnProceed').prop('disabled', false);//find btnProceed go into properties and enable btn
