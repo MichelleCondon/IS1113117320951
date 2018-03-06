b9e1ef93903f9f910f2fb2d01b9d89e826b6bfe9
Michelle Condon <117320951@umail.ucc.ie>
2018-03-06 14:12:10 +0000

    update 1

--- ebusiness/Ebus2.php
+++ ebusiness/Ebus2.php
@@ -28,17 +28,17 @@ session_start();
             <!--Visa option-->
             <label for="visa" class="visalabel">
                   <input type="radio" name="payment" id="visa" checked onClick="disablebtnProceed()"/>
-                  <img src="http://www.travelandtourismnews.com/wp-content/uploads/2008/07/logo-visa.jpg" width="100" height="70" required>
+                  <img src="http://www.travelandtourismnews.com/wp-content/uploads/2008/07/logo-visa.jpg" alt="visa logo" width="100" height="70">
               </label>
             <!--Mastercard option-->
             <label for="mastercard" class="mclabel">
                   <input type="radio" name="payment" id="mastercard" checked onClick="disablebtnProceed()"/>
-                  <img src="http://cdn.blogosfere.it/virtualearn/images/mastercard.png" width="100" height="70" required>
+                  <img src="http://cdn.blogosfere.it/virtualearn/images/mastercard.png" alt="mastercard logo" width="100" height="70">
               </label>
               <!--paypal option-->
               <label for="paypal">
                   <input type="radio" name="payment" id="paypal" checked onClick="disablebtnProceed()"/>
-                  <img src="https://blogs-images.forbes.com/mfonobongnsehe/files/2014/09/paypal.jpg" width="100" height="70" required>
+                  <img src="https://blogs-images.forbes.com/mfonobongnsehe/files/2014/09/paypal.jpg" alt="paypal logo" width="100" height="70">
               </label>
               <br/>
               <br/>
@@ -48,22 +48,22 @@ session_start();
               <center>
               <label for="cardnumber" class="lcardnumber">Card Number:</label>
               <!--text box-->
-              <input type="number" name="cardnumber" id="cardnumber" placeholder="Card Number" maxlength="16" required=''style="width:300px; height=30px font-size:22px;">
+              <input type="number" name="cardnumber" id="cardnumber" placeholder="Card Number" maxlength="16" required='' style="width:300px; height:30px font-size:22px;">
               <br/>
               <!--card name label-->
               <label for="cardname" class="lcardname">Name on Card:</label>
               <!--text box-->
-              <input type="text" name="cardname" id="cardname" placeholder="Name on Card" maxlength="20" required='' style="width:300px; height=30px font-size:22px;">
+              <input type="text" name="cardname" id="cardname" placeholder="Name on Card" maxlength="20" required='' style="width:300px; height:30px font-size:22px;">
               <br/>
               <!--security card label-->
               <label for="cvc" class="lcvc">Security Code:</label>
               <!--text box-->
-              <input type="number" name="cvc" id="security_code" placeholder="Security Code" maxlength="3" required='' style="width:300px; height=50px font-size:40px;">
+              <input type="number" name="cvc" id="security_code" placeholder="Security Code" required='' style="width:300px; height:50px font-size:40px;">
               <br/>
               <!--pin label-->
               <label for="user_pin" class="lpin">PIN:</label>
               <!--text box-->
-              <input type="password" id="user_pin" placeholder="Card PIN" maxlength="4" required='' style="width:300px; height=30px font-size:22px;">
+              <input type="password" id="user_pin" placeholder="Card PIN" maxlength="4" required='' style="width:300px; height:30px font-size:22px;">
               <!--javascript function-->
               <!--code obtained from https://www.w3schools.com/jsref/jsref_isnan.asp-->
               <script>
@@ -83,36 +83,36 @@ session_start();
               <!--name label-->
               <label for="name" class="lname">Name:</label>
               <!--text  box-->
-              <input type="text" name="name" id="name" placeholder="Name" maxlength="20" required='' style="width:300px; height=30px font-size:22px;">
+              <input type="text" name="name" id="name" placeholder="Name" maxlength="20" required='' style="width:300px; height:30px font-size:22px;">
               
               <br/>
               <!--email label-->
               <label for="email" class="lemail">Email:</label>
               <!--text box-->
-              <input type="email" name="email" id="email" placeholder="Email" required='' style="width:300px; height=30px font-size:22px;">
+              <input type="email" name="email" id="email" placeholder="Email" required='' style="width:300px; height:30px font-size:22px;">
               <br/>
               <!--heading-->
               <h4><strong>Billing address</strong></h4>
               <!--label-->
               <label for="address1" class="add1">Address Line 1:</label>
               <!--text  box-->
-              <input type="text" name="address" id="address_1" placeholder="Address Line 1" required='' style="width:300px; height=30px font-size:22px;">
+              <input type="text" name="address" id="address_1" placeholder="Address Line 1" required='' style="width:300px; height:30px font-size:22px;">
               <!--label-->
-              <label for="address2" class="add2">Address Line 2:</label>
+              <label name="address2" class="add2">Address Line 2:</label>
               <!--text  box-->
-              <input type="text" name="addresstwo" id="address_2" placeholder="Address Line 2" required='' style="width:300px; height=30px font-size:22px;">
+              <input type="text" name="addresstwo" id="address_2" placeholder="Address Line 2" required='' style="width:300px; height:30px font-size:22px;">
               <!--label-->
-              <label for="city" class="city">City:</label>
+              <label name="city" class="city">City:</label>
               <!--text  box-->
-              <input type="text" name="city" id="city1" placeholder="City" required='' style="width:300px; height=30px font-size:22px;">
+              <input type="text" name="city" id="city1" placeholder="City" required='' style="width:300px; height:30px font-size:22px;">
               <!--label-->
-              <label for="postcode" class="postcode">Post Code:</label>
+              <label name="postcode" class="postcode">Post Code:</label>
               <!--text  box-->
               <input type="text" name="post" id="post1" placeholder="Post Code" required='' style="width:300px; height=30px font-size:22px;">
               <!--label-->
-              <label for="country" class="country">Country:</label>
+              <label nME="country" class="country">Country:</label>
               <!--text  box-->
-              <input type="text" name="country" id="country1" placeholder="Country" required='' style="width:300px; height=30px font-size:22px;">
+              <input type="text" name="country" id="country1" placeholder="Country" required='' style="width:300px; height:30px font-size:22px;">
               
              </center>
              <br/>
